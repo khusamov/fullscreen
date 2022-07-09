5c3793273adf23a11ed03fdbca3978d37a07ea52
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-09 15:13:30 +0300

    Добавлен пример вызова полноэкранного режима

--- packages/fullscreen/src/components/app/Application.tsx
+++ packages/fullscreen/src/components/app/Application.tsx
@@ -1,9 +1,25 @@
 import {ApplicationStyle} from './Application.module.scss'
+import {useRef} from 'react'
 
 export function Application() {
+	const divRef = useRef<HTMLDivElement>(null)
+	const toggleFullScreen = async () => {
+		if (document.fullscreenElement) {
+			await document.exitFullscreen()
+		} else {
+			if (divRef.current) {
+				await divRef.current.requestFullscreen({navigationUI: 'hide'})
+			}
+		}
+	}
+	const onClick = async () => {
+		await toggleFullScreen()
+	}
 	return (
-		<div className={ApplicationStyle}>
-			Тестирование Fullscreen API
+		<div ref={divRef} className={ApplicationStyle}>
+			<div>Тестирование Fullscreen API</div>
+			<div>Fullscreen Enabled = {document.fullscreenEnabled ? 'true' : 'false'}</div>
+			<div><button onClick={onClick}>Fullscreen</button></div>
 		</div>
 	)
 }
