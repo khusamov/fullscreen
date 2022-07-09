d358571726c4ba801d07ebbda372f18f5beaff83
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-09 12:55:57 +0300

    Начало проекта

---
+++ packages/fullscreen/src/components/app/Application.tsx
@@ -0,0 +1,9 @@
+import {ApplicationStyle} from './Application.module.scss'
+
+export function Application() {
+	return (
+		<div className={ApplicationStyle}>
+			Тестирование Fullscreen API
+		</div>
+	)
+}
